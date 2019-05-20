29ac684752e4ffcb91c8e86942044242b10e4561
Pedro Coelho <pedro.coelho@dcomp.sor.ufscar.br>
2019-05-20 15:03:49 -0300

    adicionado um código inicial

--- Compiler.java
+++ Compiler.java
@@ -3,3 +3,69 @@
     // Pablo Laranjo           726577
     // Pedro Coelho            743585
     // Vinícius Crepschi       743601
+
+//    comp2
+
+/*
+Expr ::= '(' oper  Expr Expr ')' | number
+oper ::= '+' | '-'
+number ::= '0' | ... | '9'
+
+	 Example of program:
+    
+         (+ (- 5 4) 2)
+*/
+
+/*
+ - um metodo para cada nao-terminal -> analisador sintatico (parser)
+ - error -- aponta o erro (quando ocorrer)
+ - nextToken (analisador lexico) -- posicao do proximo token valido 
+ */
+
+public class Compiler {
+
+    public int compile( char []p_input ) {
+		int resultado;        
+		input = p_input;
+        tokenPos = 0;
+        nextToken();
+        resultado = expr();
+		    
+	    if ( token != '\0' )
+          error();
+
+		return resultado;
+    }
+
+    private void nextToken() {
+		
+		while(tokenPos < input.length && input[tokenPos] == ' '){
+			tokenPos++;
+		}
+
+		if(tokenPos >= input.length)
+			token = '\0';
+		else {
+			token = input[tokenPos];
+			tokenPos++;
+		}
+    }
+    
+    private void error() {
+        if ( tokenPos == 0 ) 
+          tokenPos = 1; 
+        else 
+          if ( tokenPos >= input.length )
+            tokenPos = input.length;
+        
+        String strInput = new String( input, tokenPos - 1, input.length - tokenPos + 1 );
+        String strError = "Error at \"" + strInput + "\"";
+        System.out.println( strError );
+        throw new RuntimeException(strError);
+    }
+    
+    private char token;
+    private int  tokenPos;
+    private char []input;
+      
+}
--- Main.java
+++ Main.java
@@ -3,3 +3,14 @@
     // Pablo Laranjo           726577
     // Pedro Coelho            743585
     // Vinícius Crepschi       743601
+
+public class Main {
+    public static void main( String []args ) {
+        char []input = "------".toCharArray();
+        
+        Compiler compiler = new Compiler();
+        //compiler.compile(input);
+        System.out.println(compiler.compile(input));        
+    }
+}
+        
