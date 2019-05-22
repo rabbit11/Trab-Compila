5eb95101b7abdaa632be2abb8ef32b1f9eebde94
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-05-22 17:01:40 -0300

    Return Stat done

---
+++ AST/ReturnStat.java
@@ -0,0 +1,20 @@
+package AST;
+
+public class ReturnStat extends Stat {
+    public ReturnStat(Variable v, Expr e) {
+        super(v, e);
+    }
+
+    @Override
+    public void genC(PW pw) {
+        pw.printBL(); // Quebra de linha
+
+        pw.printI(); // Ident
+        pw.printNI("return "); // Return
+
+        super.getExpr().genC(pw); // Expressão
+        pw.printNI(";"); // ';'
+
+        pw.printBL(); // Quebra de linha
+    }
+}
