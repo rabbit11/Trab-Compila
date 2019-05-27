0614462d8556fdc7419ae96f1c611d88f93f3805
Pedro Coelho <pedro.coelho@dcomp.sor.ufscar.br>
2019-05-27 15:42:24 -0300

    consertei algumas coisas no paramList

--- AST/ParamList.java
+++ AST/ParamList.java
@@ -11,13 +11,24 @@ public class ParamList extends ParamDec {
     }
 
     public void genC(PW pw) {
+        int i = 0;
+
         for (ParamDec p : arrayParam) {
             p.genC(pw);
+            i++;
+        }
+
+        if(i > 0 && i < arrayParam.size()){//garante que imprimimos , apenas quando tem mais de 1 param
+            pw.printNI(",");               //e não imprimimos após o último
         }
     }
 
     public void setArrayParam(ArrayList<ParamDec> p) {
         this.arrayParam = p;
     }
-}
 
+    public ArrayList<ParamDec> getListaStats(){
+      return this.arrayParam;
+    }
+    
+}
