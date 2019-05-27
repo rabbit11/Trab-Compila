a8fb6485209dac940ca5ff07d0993b0608b94be6
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-05-27 18:50:31 -0300

    ExprLiteral Done

--- AST/ExprLiteral.java
+++ AST/ExprLiteral.java
@@ -1,3 +1,22 @@
 package AST
 
 //ExprLiteral ::= LiteralInt | LiteralBoolean | LiteralString
+public class ExprLiteral extends Expr {
+    private Expr exp;
+
+    public ExprLiteral(Expr exp) {
+        this.setExp(exp);
+    }
+
+    public void genC(PW pw) {
+    }
+
+    public void setExp(Expr exp) {
+        this.exp = exp;
+    }
+
+    public Expr getExp() {
+        return this.exp;
+    }
+
+}
