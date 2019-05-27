06c9b5d7061ca8df87fd492ea0bb4a4249a6aae1
Pedro Coelho <pedro.coelho@dcomp.sor.ufscar.br>
2019-05-27 16:57:57 -0300

    ExprRel e ExprAnd done

--- AST/ExprRel.java
+++ AST/ExprRel.java
@@ -1,3 +1,51 @@
 package AST
 
 //ExprRel ::= ExprAdd [ RelOp ExprAdd ]
+
+public class ExprRel extends Expr{
+    
+    private Expr esquerda, direita;
+    private Symbol operador;
+
+    public ExprRel(Expr esq, Expr dir, Symbol op){
+        this.setDireita(dir);
+        this.setEsquerda(esq);
+        this.setOperador(op);
+    }
+
+    public void genC(PW pw){
+        esquerda.genC(pw);
+
+        if(this.operador != null){
+            pw.printNI(" " + operador.toString() + " ");
+        }
+
+        if (this.direita != null) {
+            direita.genC(pw);
+        }
+    }
+
+    public void setEsquerda(Expr esq) {
+        this.esquerda = esq;
+    }
+
+    public void setDireita(Expr dir) {
+        this.direita = dir;
+    }
+
+    public void setOperador(Symbol op) {
+        this.operador = op;
+    }
+
+    public Expr getEsquerda() {
+        return this.esquerda;
+    }
+
+    public Expr getDireita() {
+        return this.direita;
+    }
+
+    public Symbol getOperador() {
+        return this.operador;
+    }
+}
