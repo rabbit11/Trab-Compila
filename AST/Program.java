3f64cfa8be497ac84514f4bf05edfceccdd82e23
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-05-28 14:38:02 -0300

    nao tenho certeza sobre as mudanças no program

--- AST/Program.java
+++ AST/Program.java
@@ -2,28 +2,53 @@ package AST;
 
 import java.util.ArrayList;
 
-//Program ::= Func {F unc}
+//Program ::= Func {Func}
 
 public class Program {
     private ArrayList<Func> arrayFunc;
+    private StatList statList;
 
-    public Program(ArrayList<Func> arrayFunc) {
+    public Program(ArrayList<Func> arrayFunc, StatList statList) {
         this.arrayFunc = arrayFunc;
+        this.statList = statList;
     }
 
     public void genC(PW pw) {
-        pw.println("#include <stdio.h>\n");
-
-        // Se não tiver nenhuma função declarada
-        if (arrayFunc.contains(null)) {
-            pw.println("int main(){}");
+        pw.out.println("#include <stdio.h>");
+        pw.out.println();
+        pw.println("void main() {");
+        pw.add();
+        // generate code for the declaration of variables
+        for (VarDecStat var : arrayVariable){
+            pw.println(var.getType().getCname() + " " + var.getName() + ";");
         }
-
-        // Pode ter funções declaradas
-        else {
-            for (Func f : arrayFunc) {
-                f.genC(pw);
-            }
-        }
+        pw.out.println("");
+        statList.genC(pw);
+        pw.sub();
+        pw.out.println("}");
     }
 }
+
+// public class Program {
+//     private ArrayList<Func> arrayFunc;
+
+//     public Program(ArrayList<Func> arrayFunc) {
+//         this.arrayFunc = arrayFunc;
+//     }
+
+//     public void genC(PW pw) {
+//         pw.println("#include <stdio.h>\n");
+
+//         // Se não tiver nenhuma função declarada
+//         if (arrayFunc.contains(null)) {
+//             pw.println("int main(){}");
+//         }
+
+//         // Pode ter funções declaradas
+//         else {
+//             for (Func f : arrayFunc) {
+//                 f.genC(pw);
+//             }
+//         }
+//     }
+// }
--- AST/Type.java
+++ AST/Type.java
@@ -7,23 +7,35 @@ import Lexer.*;
 
 //Type ::= "Int" | "Boolean" | "String"
 
-public class Type {
+abstract public class Type {
   private Symbol type;
+  private String name;
 
-  public Type(Symbol a) {
-    this.type = a;
+  public Type(String name) {
+    this.name = name;
   }
 
-  public void tipo(PW pw){
-    if(type == Symbol.INT)
-      pw.println("int");
-    else if (type == Symbol.BOOLEAN)
-     pw.println("Boolean");
-    else if (type == Symbol.STRING)
-      pw.println("String");
-  }
+  public static Type booleanType = new BooleanType();
+  public static Type integerType = new IntegerType();
+  public static Type charType = new CharType();
 
+  
+  abstract public String getCname();
+  
+  public String getName() {
+    return name;
+  }
+  
   public Symbol getType(){
     return this.type;
   }
+  
+        // public void tipo(PW pw){
+        //   if(type == Symbol.INT)
+        //     pw.println("int");
+        //   else if (type == Symbol.BOOLEAN)
+        //    pw.println("Boolean");
+        //   else if (type == Symbol.STRING)
+        //     pw.println("String");
+        // }
 }
