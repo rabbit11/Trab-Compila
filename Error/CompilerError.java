48d63d1d9b8a9d78f2d39eb03ed695d62aeec25c
Pedro Coelho <pedro.coelho@dcomp.sor.ufscar.br>
2019-06-15 19:19:19 -0300

    criada classe error e seu diretorio

---
+++ Error/CompilerError.java
@@ -0,0 +1,10 @@
+//     Nome                    RA
+// Bruno Asti Baradel      726499
+// Pablo Laranjo           726577
+// Pedro Coelho            743585
+// Vinícius Crepschi       743601
+
+package Error;
+
+import Lexer.*;
+import java.io.*;
