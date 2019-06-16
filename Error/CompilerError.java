7a9219aac08296c9bc12c3c2381c7222a542c377
Pedro Coelho <pedro.coelho@dcomp.sor.ufscar.br>
2019-06-16 13:53:35 -0300

    classe error finalizada

--- Error/CompilerError.java
+++ Error/CompilerError.java
@@ -8,3 +8,40 @@ package Error;
 
 import Lexer.*;
 import java.io.*;
+
+import AST.PW;
+
+public class CompilerError{
+    
+    public CompilerError(PW pw, String fileName){
+        this.pw = pw;
+        this.fileName = fileName;
+        this.signaled = false;
+    }
+
+    public void setLexer(Lexer lexer){
+        this.lexer = lexer;
+    }
+
+    public boolean errorSignaled(){
+        return signaled;
+    }
+
+    public void Error(String message){
+        if(errorSignaled()){
+            pw.println(message);
+        }
+        else {
+            String errorMessage = this.fileName + ": " + lexer.getLineNumber() + message;
+            
+            pw.println(errorMessage);
+            pw.println(lexer.getCurrentLine());
+
+            this.signaled = true;
+        }
+    }
+
+    private PW pw;
+    private String fileName;
+    private boolean signaled;
+}
